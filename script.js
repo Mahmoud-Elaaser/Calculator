7c3bcb65cb7a29f5f9e71e80b22ca7f396a74cf4
Mahmoud Elaaser <mahmoudelaaser57@gmail.com>
2024-06-26 20:05:38 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+let display = document.getElementById('display');
+
+function clearDisplay() {
+    display.textContent = '0';
+}
+
+function deleteLast() {
+    display.textContent = display.textContent.slice(0, -1) || '0';
+}
+
+function appendToDisplay(value) {
+    if (display.textContent === '0' && value !== '.') {
+        display.textContent = value;
+    } else {
+        display.textContent += value;
+    }
+}
+
+function calculateResult() {
+    try {
+        display.textContent = eval(display.textContent);
+    } catch {
+        display.textContent = 'Error';
+    }
+}
